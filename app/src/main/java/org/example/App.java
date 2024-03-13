7327c109d3496abb14938b8b2ece578a634bbec4
BCemreD <cemredisli@hotmail.com>
2024-03-13 13:01:38 +0300

    ekledim

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+public class App {
+//    public String getGreeting() {
+//        return "Hello World!";
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(new App().getGreeting());
+//    }
+	
+	
+}
